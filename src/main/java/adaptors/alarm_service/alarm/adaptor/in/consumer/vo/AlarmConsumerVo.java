b78851ad287cffcba8a9dc6b6a6d135d91ae6693
Baek Seung Yeop <tmdduq1999@naver.com>
2024-11-28 17:52:06 +0900

    [FEAT] : alarm create

--- src/main/java/adaptors/alarm_service/alarm/adaptor/in/consumer/vo/AlarmConsumerVo.java
+++ src/main/java/adaptors/alarm_service/alarm/adaptor/in/consumer/vo/AlarmConsumerVo.java
@@ -2,8 +2,10 @@ package adaptors.alarm_service.alarm.adaptor.in.consumer.vo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class AlarmConsumerVo {
 
     @JsonProperty("menteeUuid")
